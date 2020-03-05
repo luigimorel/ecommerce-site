d11e6ff8647015bb8fe963e6264a4c34da835e3f
morelmiles <morelmiles@gmail.com>
2020-03-05 12:58:34 +0300

    Added the user MONGODBuser schema

---
+++ controllers/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true, maxlength: 32, required: true },
+    name: { type: String, trim: true, unique: 32, required: true },
+    hashed_password: { type: String, required: true },
+    about: { type: String, trim: true },
+    salt: String,
+    role: { type: Number, default: 0 },
+    history: { type: Array, default: [] }
+  },
+  { timestamps: true }
+);
+
+//Virtual fields
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if (!password) return "";
+
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
